42245cf01a201c1c02bd237db0ed0421f83f5c79
Andrew_Time <34838425+shhh-hoo@users.noreply.github.com>
2026-08-17 17:30:18 +0800

    export hardened protocol

--- src/protocol/index.ts
+++ src/protocol/index.ts
@@ -1,16 +1,40 @@
-export type {
-  CapabilityCoverage,
-  CapabilityFitResult,
-  ComponentCapabilityInspector,
-  ComponentCapabilityManifest,
-  ComponentRecommendedAction,
-  LearningAction,
-  LearningRequestDescriptor,
+export {
+  COMPONENT_PROTOCOL_VERSION,
 } from "./capability";
 
 export type {
-  CapabilityRunPurpose,
+  CapabilityCoverage,
+  CapabilityDescriptor,
+  CapabilityExecutionModel,
+  CapabilityFitResult,
+  ComponentCapabilityInspector,
+  ComponentCapabilityManifest,
+  ComponentProtocolVersion,
+  CoreLearningAction,
+  ExtensionLearningAction,
+  InteractiveControl,
+  LearningAction,
+  LearningRequestDescriptor,
+  SchemaReference,
+} from "./capability";
+
+export type {
+  CapabilityRunMode,
+  ComponentControlMessage,
+  ComponentEvent,
+  ComponentEventType,
+  CoreComponentEventType,
+  ExtensionComponentEventType,
   LearningCapabilityExecution,
   LearningCapabilityExecutionResult,
+  LearningCapabilityExecutionStatus,
   LearningCapabilityRuntime,
+  ProtocolIssue,
+  SchemaBoundPayload,
 } from "./runtime";
+
+export {
+  assertComponentEventConforms,
+  assertExecutionConforms,
+  assertManifestConforms,
+} from "./conformance";
